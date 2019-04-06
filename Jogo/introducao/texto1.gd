6464e8e9161ec7404ec8ba25b354752be772ec20
Washington Pagotto Batista <wstroks@gmail.com>
2019-04-06 03:05:21 -0300

    vai miseria

--- Jogo/introducao/texto1.gd
+++ Jogo/introducao/texto1.gd
@@ -5,7 +5,7 @@ var a= 1
 # var b = "textvar"
 var ALIGN_CENTER = 1
 var page=0
-var dialogo=["1","tema 1", "tema 2", "tema22", "tema3","tema33","tema4","tema44","tema5","tema55"]
+var dialogo=["Há muito tempo atrás nasceu um homem em Florença.","Até os 24 anos, era um homem comum, até entrar numa caverna e encontrar seres extraterrestres.","Após este evento, este homem se tornou… LEONARDO DA VINCI","Da Vinci deixou escritos em seu diário..", "Porém as páginas que continham seus relatos da caverna foram… A-R-R-A-N-C-A-D-A-S! !!!!!!", "Uma tarde, o caderno com os escritos de Leonardo da Vinci caiu nos colos do jovem artista Julielson.", "Ao se deparar com as páginas da transformação arrancadas, Julielson resolve ir ao museu da Vinci para solucionar o Mistério da Caverna.","tema33","tema4","tema44","tema5","tema55"]
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -24,13 +24,13 @@ func _input(event):
 				page+=1
 				print(page)
 				
-				if(page==2):
+				if(page==3):
 					get_parent().get_node("ParallaxBackground/ParallaxLayer/TextureFrame").set_texture(load("res://sprites/2-diario.png"))
-				if(page==4):
+				if(page==5):
 					get_parent().get_node("ParallaxBackground/ParallaxLayer/TextureFrame").set_texture(load("res://sprites/3-cave.png"))
-				if(page==6):
+				if(page==10):
 					get_parent().get_node("ParallaxBackground/ParallaxLayer/TextureFrame").set_texture(load("res://sprites/4-ovni.png"))
-				if(page==8):
+				if(page==15):
 					get_parent().get_node("ParallaxBackground/ParallaxLayer/TextureFrame").set_texture(load("res://sprites/5-boyzinho.png"))
 				set_bbcode(dialogo[page])
 				set_visible_characters(0)
